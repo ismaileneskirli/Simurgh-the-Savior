86cfd0d0d70ce0620fd704f2b386f0b4c72926f2
ismaileneskirli <69651222+ismaileneskirli@users.noreply.github.com>
2021-03-19 23:27:19 +0300

    simurgh can fly

--- Simurgh-the-Savior/Assets/Scripts/PlayerController.cs
+++ Simurgh-the-Savior/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // vars for moving the player
+    float xOffset ;
+    float yOffset ;
+    [SerializeField] float controlSpeed = 15f;
+
+    //var for restricting player movement
+    [SerializeField] float xRange = 12f;
+    [SerializeField] float yRange = 2.5f;
     // Start is called before the first frame update
+    // pitch = eğim
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField]float controlPitchFactor = -15f;
+    [SerializeField] float positionYawFactor = 10f;
+    [SerializeField] float controlRollFactor = -10f;
+    float xThrow;
+    float yThrow;
     void Start()
     {
 
@@ -13,8 +29,50 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControl  = yThrow * controlPitchFactor;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+
+        float rollDueToControl = xThrow * controlRollFactor;
+
+        // Quaternion related to rotation
+        float pitch = pitchDueToControl + pitchDueToPosition;
+        float yaw = yawDueToPosition;
+        float roll = rollDueToControl;
+        //transform.localRotation = Quaternion.Euler(x, y, z);
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
 
     }
+
+    void ProcessTranslation()
+    {
+        // XPOS
+        xThrow = Input.GetAxis("Horizontal");
+        xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        // restricts the movement with given range.
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+
+        // YPOS
+        yThrow = Input.GetAxis("Vertical");
+        yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange + 10f);
+
+
+        // Change the position with given values.
+        transform.localPosition = new Vector3
+        (clampedXPos,
+         clampedYPos,
+        transform.localPosition.z);
+    }
+
 }
